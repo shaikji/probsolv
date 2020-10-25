df1cc92c1e1d59ac7f9992e462f6cddd82ce3d58
Jilani <sjilani2014@gmail.com>
2020-10-25 15:22:22 +0530

    AVP: Playlists: Binary Search on Answer : Allocate Pages of Books

---
+++ src/com/jilani/ds/avp/binarysearch/bsonanswer/AllocatePagesOfBook.java
@@ -0,0 +1,65 @@
+package com.jilani.ds.avp.binarysearch.bsonanswer;
+
+public class AllocatePagesOfBook {
+
+	public static void main(String[] args) {
+
+		int[] books = { 10, 20, 30, 40 };
+		int s = 2;
+		System.out.println(" Min # of Pages per student = " + allocatePages(books, s));
+	}
+
+	static int allocatePages(int[] books, int s) {
+
+		int max = 0;
+		int sum = 0;
+
+		for (int elem : books) {
+			max = Math.max(max, elem);
+			sum += elem;
+		}
+
+		return binsearch(books, max, sum, s);
+	}
+
+	static int binsearch(int[] books, int l, int r, int s) {
+
+		int ans = -1;
+
+		while (l <= r) {
+
+			int mid = l + (r - l) / 2;
+
+			int n = partition(books, mid);
+
+			if (n == s) {
+				ans = mid;
+				r = mid - 1;
+			} else if (n > s) {
+				l = mid + 1;
+			} else {
+				r = mid - 1;
+			}
+		}
+
+		return ans;
+	}
+
+	static int partition(int[] arr, int sum) {
+
+		int n = 1;
+		int currSum = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			if (currSum + arr[i] <= sum) {
+				currSum += arr[i];
+			} else {
+				n++;
+				currSum = arr[i];
+			}
+		}
+
+		return n;
+	}
+
+}
--- src/com/jilani/ds/avp/binarysearch/bsonanswer/FindPeakElement.java
+++ src/com/jilani/ds/avp/binarysearch/bsonanswer/FindPeakElement.java
@@ -5,63 +5,92 @@ import com.jilani.ds.utils.Utils;
 public class FindPeakElement {
 
 	public static void main(String[] args) {
-		
-		FindPeakElement app = new FindPeakElement();
-		int[] arr = {5, 10, 20,15 };
-		Utils.printArray(arr);
-		System.out.println(" Peak element = " + app.findPeak(arr));
 
-		int[] arr2 = {10, 20, 15, 2, 23, 90, 67 };
+		FindPeakElement app = new FindPeakElement();
+		int[] arr = { 5, 10, 20, 15 };
+		Utils.printArray(arr);
+		System.out.println(" Peak element index = " + app.findPeak(arr));
+		System.out.println(" Peak element index = " + app.findPeakIndex(arr));
+		
+
+		int[] arr2 = { 10, 20, 15, 2, 23, 90, 67 };
 		Utils.printArray(arr2);
-		System.out.println(" Peak element = " + app.findPeak(arr2));
-		
+		System.out.println(" Peak element Index = " + app.findPeak(arr2));
+		System.out.println(" Peak element Index = " + app.findPeakIndex(arr2));
+
 	}
-	
-	int findPeak(int[] arr) {
-		
-		if ( arr == null || arr.length == 0 )
+
+	int findPeakIndex(int[] arr) {
+
+		if (arr == null || arr.length == 0) {
 			return -1;
-		
-		return binsearch_on_answer_v2(arr, 0, arr.length-1);
+		}
+
+		int l = 0;
+		int r = arr.length - 1;
+
+		while (l <= r) {
+
+			int mid = l + (r - l) / 2;
+
+			if (mid == 0 || mid == arr.length - 1)
+				return mid;
+
+			else if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1])
+				return mid;
+			else if (arr[mid + 1] > arr[mid])
+				l = mid + 1;
+			else
+				r = mid - 1;
+		}
+
+		return -1;
 	}
-	
+
+	int findPeak(int[] arr) {
+
+		if (arr == null || arr.length == 0)
+			return -1;
+
+		return binsearch_on_answer_v2(arr, 0, arr.length - 1);
+	}
+
 	int binsearch_on_answer(int[] arr, int start, int end) {
-		
-		while ( start <= end) {
-			
-			int mid = start + ( end - start)/2;
-			
+
+		while (start <= end) {
+
+			int mid = start + (end - start) / 2;
+
 			boolean left = true;
-			if ( mid > 0 && arr[mid] <= arr[mid-1])
+			if (mid > 0 && arr[mid] <= arr[mid - 1])
 				left = false;
 			boolean right = true;
-			if ( mid < arr.length-1 && arr[mid] <= arr[mid+1])
+			if (mid < arr.length - 1 && arr[mid] <= arr[mid + 1])
 				right = false;
-			
-			if ( left && right)
+
+			if (left && right)
 				return mid;
 			else if (left)
 				start = mid + 1;
 			else
 				end = mid - 1;
 		}
-		
+
 		return -1;
 	}
-	
-	
+
 	int binsearch_on_answer_v2(int[] arr, int start, int end) {
-		
-		while ( start < end) {
-			
-			int mid = start + ( end - start)/2;
-			
-			if (arr[mid] > arr[mid+1])
+
+		while (start < end) {
+
+			int mid = start + (end - start) / 2;
+
+			if (arr[mid] > arr[mid + 1])
 				end = mid;
 			else
-				start = mid+1;
+				start = mid + 1;
 		}
-		
+
 		return start;
 	}
 
