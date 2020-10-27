892149401a4391b490df61dec99b0ec76bbb6259
Jilani <sjilani2014@gmail.com>
2020-10-27 15:48:21 +0530

    Merge Intervals Pattern: Merge Intervals

--- src/com/jilani/ds/educative/mergeintervals/MergeIntervals.java
+++ src/com/jilani/ds/educative/mergeintervals/MergeIntervals.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class MregeIntervals {
+public class MergeIntervals {
 
 	public static void main(String[] args) {
 
