16ac369caf782a0e96b59b3b68b2ae3980c4cad4
Jilani <sjilani2014@gmail.com>
2020-10-31 08:03:04 +0530

    Merge Intervals Pattern: Find all conflicting appointments

--- src/com/jilani/ds/educative/mergeintervals/FindAllConflictingAppointments.java
+++ src/com/jilani/ds/educative/mergeintervals/FindAllConflictingAppointments.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class NumberOfConflictingAppointments {
+public class FindAllConflictingAppointments {
 
 	public static void main(String[] args) {
 
