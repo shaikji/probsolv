3f6e6ffd58e291d3f9144383f71f82fb292e9d0e
Jilani <sjilani2014@gmail.com>
2020-10-31 21:12:00 +0530

    2Ptr - Problem #2 - Remove duplicates

--- src/com/jilani/ds/educative/twopointer/RemoveDuplicates.java
+++ src/com/jilani/ds/educative/twopointer/RemoveDuplicates.java
@@ -13,6 +13,14 @@ public class RemoveDuplicates {
 		for ( int i=0; i < len; i++) {
 			System.out.print(arr[i]+" ");
 		}
+		System.out.println("\n");
+		int[] arr2 = { 2, 3, 3, 3, 6, 9, 9 };
+		Utils.printArray(arr2);
+		System.out.println(" After removing duplicates V2 ");
+		int len2 = removeDuplicatesV2(arr2);
+		for ( int i=0; i < len2; i++) {
+			System.out.print(arr2[i]+" ");
+		}
 	}
 
 	static int removeDuplicates(int[] arr) {
@@ -29,5 +37,21 @@ public class RemoveDuplicates {
 			}
 		}
 		return i+1;
+	}
+	
+	static int removeDuplicatesV2(int[] arr) {
+
+		if (arr == null || arr.length == 0)
+			return 0;
+
+		int nextNonDuplicate = 1; // Index of the next non duplicate element
+		
+		for ( int i=1; i < arr.length; i++) {
+			if ( arr[i] != arr[nextNonDuplicate-1]) {
+				arr[nextNonDuplicate] = arr[i];
+				nextNonDuplicate++;
+			}
+		}
+		return nextNonDuplicate;
 	}
 }
