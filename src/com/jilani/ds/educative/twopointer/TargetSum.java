1e2b24f6df8fac3840f38d974c89df01994df3ab
Jilani <sjilani2014@gmail.com>
2020-10-31 20:51:07 +0530

    Two Pointer Approach - Find pair with TargetSum

--- src/com/jilani/ds/educative/twopointer/TargetSum.java
+++ src/com/jilani/ds/educative/twopointer/TargetSum.java
@@ -1,10 +1,40 @@
 package com.jilani.ds.educative.twopointer;
 
+import java.util.Arrays;
+
+import com.jilani.ds.utils.Utils;
+
 public class TargetSum {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		int[] arr = { 1, 2, 3, 4, 6 };
+		int targetSum = 6;
+		Utils.printArray(arr);
+		System.out.println(" Pair Indexes =  " + Arrays.toString(findPairWithSum(arr, targetSum)));
 	}
 
+	static int[] findPairWithSum(int[] arr, int targetSum) {
+
+		if (arr == null || arr.length == 0)
+			return new int[] { -1, -1 };
+
+		int i = 0;
+		int j = arr.length - 1;
+
+		while (i < j) {
+
+			if (arr[i] + arr[j] == targetSum) {
+				return new int[] { i, j };
+			} else if (arr[i] + arr[j] < targetSum) {
+				i++;
+			} else {
+				j--;
+			}
+		}
+
+		// For single element, there is no pair.so return -1.
+		return new int[] { -1, -1 };
+
+	}
 }
